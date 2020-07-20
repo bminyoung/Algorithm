3d77cb9c248fd3599e3ef69e9b283ead681bbac2
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-07-20 17:51:29 +0900

    200720_solve

--- src/me/nyoung/BOJ/MST/sol_1197_2.java
+++ src/me/nyoung/BOJ/MST/sol_1197_2.java
@@ -8,11 +8,11 @@ public class sol_1197_2 {
         Scanner sc = new Scanner(System.in);
         int V = sc.nextInt();
         int E = sc.nextInt();
-        PriorityQueue<Vertex> vertices = new PriorityQueue<>();
+        PriorityQueue<Edge> vertices = new PriorityQueue<>();
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[V+1];
 
-        List<Vertex>[] adj = new ArrayList[V+1];
+        List<Edge>[] adj = new ArrayList[V+1];
         for (int i = 1; i <= V; i++) {
             adj[i] = new ArrayList<>();
         }
@@ -20,8 +20,8 @@ public class sol_1197_2 {
             int from = sc.nextInt();
             int to = sc.nextInt();
             int weight = sc.nextInt();
-            adj[from].add(new Vertex(from, to, weight));
-            adj[to].add(new Vertex(to, from, weight));
+            adj[from].add(new Edge(from, to, weight));
+            adj[to].add(new Edge(to, from, weight));
         }
 
         queue.add(1); //set start vertex
@@ -31,38 +31,21 @@ public class sol_1197_2 {
             int std = queue.poll();
             visited[std] = true;
             for (int i = 0; i < adj[std].size(); i++) {
-                if(!visited[adj[std].get(i).end]){
+                if(!visited[adj[std].get(i).v2]){
                     vertices.add(adj[std].get(i));
                 }
             }
 
             // weight가 가장 적은 간선을 꺼내서 확인하고 추가한다
             while(vertices.size() != 0){
-                Vertex v = vertices.poll();
-                if(!visited[v.end]){
-                    queue.add(v.end);
-                    weightSum += v.dist;
+                Edge v = vertices.poll();
+                if(!visited[v.v2]){
+                    queue.add(v.v2);
+                    weightSum += v.weight;
                     break;
                 }
             }
         }
         System.out.println(weightSum);
-    }
-}
-
-class Vertex implements Comparable<Vertex>{
-    int start;
-    int end;
-    int dist;
-
-    public Vertex(int start, int end, int dist) {
-        this.start = start;
-        this.end = end;
-        this.dist = dist;
-    }
-
-    @Override
-    public int compareTo(Vertex v) {
-        return this.dist - v.dist;
     }
 }
